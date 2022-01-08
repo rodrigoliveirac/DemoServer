60fa60a9a73e295f2e986d02183499c9740cde09
Rodrigo de Oliveira Cavalcante <rodrigoliveiracavalcante@gmail.com>
2022-01-08 16:24:55 -0300

    inital commit

---
+++ src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,16 @@
+package com.example
+
+import com.example.plugins.configureRouting
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080) {
+        module()
+    }
+}
+
+fun Application.module() {
+    configureRouting()
+}
